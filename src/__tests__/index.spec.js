ef5ca1cdd00a86d978774adaa346927bf28434eb
Tom Chen <developer@tomchentw.com>
2017-10-21 12:09:24 +0800

    test(index.js): migrate to jest

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,58 +1,45 @@
-import {
-  resolve as resolvePath,
-} from "path";
-
-import {
-  default as test,
-} from "tape";
-
-import {
-  default as MemoryFS,
-} from "memory-fs";
-
-import {
-  default as webpack,
-} from "webpack";
-
-import {
-  default as UnusedFilesWebpackPlugin,
-} from "../index";
+import path from "path";
+import MemoryFS from "memory-fs";
+import webpack from "webpack";
+import UnusedFilesWebpackPlugin from "../index";
 
 const EXPECTED_FILENAME_LIST = [
   `CHANGELOG.md`,
   `README.md`,
   `src/__tests__/index.spec.js`,
-  `package.json`,
+  `package.json`
 ];
 
-test(`UnusedFilesWebpackPlugin`, t => {
-  const compiler = webpack({
-    context: resolvePath(__dirname, `../../`),
-    entry: {
-      UnusedFilesWebpackPlugin: resolvePath(__dirname, `../index.js`),
-    },
-    output: {
-      path: __dirname, // It will be in MemoryFS :)
-    },
-    plugins: [new UnusedFilesWebpackPlugin()],
-  });
-  compiler.outputFileSystem = new MemoryFS();
+describe(`UnusedFilesWebpackPlugin module`, () => {
+  it(`should work as expected`, done => {
+    const compiler = webpack({
+      context: path.resolve(__dirname, `../../`),
+      entry: {
+        UnusedFilesWebpackPlugin: path.resolve(__dirname, `../index.js`)
+      },
+      output: {
+        path: __dirname // It will be in MemoryFS :)
+      },
+      plugins: [new UnusedFilesWebpackPlugin()]
+    });
+    compiler.outputFileSystem = new MemoryFS();
 
-  compiler.run((err, stats) => {
-    t.equal(err, null);
+    compiler.run((err, stats) => {
+      expect(err).toBeFalsy();
 
-    const { warnings } = stats.compilation;
-    t.equal(warnings.length, 1);
+      const { warnings } = stats.compilation;
+      expect(warnings).toHaveLength(1);
 
-    const [unusedFilesError] = warnings;
-    t.equal(unusedFilesError instanceof Error, true);
+      const [unusedFilesError] = warnings;
+      expect(unusedFilesError).toBeInstanceOf(Error);
 
-    const { message } = unusedFilesError;
-    const containsExpected = EXPECTED_FILENAME_LIST.every(filename =>
-      message.match(filename)
-    );
-    t.equal(containsExpected, true);
+      const { message } = unusedFilesError;
+      const containsExpected = EXPECTED_FILENAME_LIST.every(filename =>
+        message.match(filename)
+      );
+      expect(containsExpected).toBeTruthy();
 
-    t.end();
+      done();
+    });
   });
 });
