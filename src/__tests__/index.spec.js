29a5983262116920037c490f30726528aed26763
Neil Kumar <neil.kumar@eigentech.com>
2020-03-27 18:55:04 +0000

    fix tests and patch for webpack4

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -11,18 +11,55 @@ const EXPECTED_FILENAME_LIST = [
 ];
 
 describe(`UnusedFilesWebpackPlugin module`, () => {
-  it(
-    `should work as expected`,
-    done => {
+  it(`should work as expected`, done => {
+    const compiler = webpack({
+      mode: "none",
+      context: path.resolve(__dirname, `../../`),
+      entry: {
+        UnusedFilesWebpackPlugin: path.resolve(__dirname, `../index.js`)
+      },
+      output: {
+        path: __dirname // It will be in MemoryFS :)
+      },
+      plugins: [new UnusedFilesWebpackPlugin()]
+    });
+    compiler.outputFileSystem = new MemoryFS();
+
+    compiler.run((err, stats) => {
+      expect(err).toBeFalsy();
+
+      const { warnings } = stats.compilation;
+      expect(warnings).toHaveLength(1);
+
+      const [unusedFilesError] = warnings;
+      expect(unusedFilesError).toBeInstanceOf(Error);
+
+      const { message } = unusedFilesError;
+      const containsExpected = EXPECTED_FILENAME_LIST.every(filename =>
+        message.match(filename)
+      );
+      expect(containsExpected).toBeTruthy();
+
+      done();
+    });
+  }, 10000);
+
+  describe(`deprecated options.pattern`, () => {
+    it(`should work as expected`, done => {
       const compiler = webpack({
+        mode: "none",
         context: path.resolve(__dirname, `../../`),
         entry: {
           UnusedFilesWebpackPlugin: path.resolve(__dirname, `../index.js`)
         },
         output: {
-          path: __dirname // It will be in MemoryFS :)
+          path: __dirname
         },
-        plugins: [new UnusedFilesWebpackPlugin()]
+        plugins: [
+          new UnusedFilesWebpackPlugin({
+            pattern: "src/**/*.*"
+          })
+        ]
       });
       compiler.outputFileSystem = new MemoryFS();
 
@@ -36,129 +73,80 @@ describe(`UnusedFilesWebpackPlugin module`, () => {
         expect(unusedFilesError).toBeInstanceOf(Error);
 
         const { message } = unusedFilesError;
-        const containsExpected = EXPECTED_FILENAME_LIST.every(filename =>
-          message.match(filename)
-        );
-        expect(containsExpected).toBeTruthy();
+        expect(message).toMatchSnapshot();
 
         done();
       });
-    },
-    10000
-  );
-
-  describe(`deprecated options.pattern`, () => {
-    it(
-      `should work as expected`,
-      done => {
-        const compiler = webpack({
-          context: path.resolve(__dirname, `../../`),
-          entry: {
-            UnusedFilesWebpackPlugin: path.resolve(__dirname, `../index.js`)
-          },
-          output: {
-            path: __dirname
-          },
-          plugins: [
-            new UnusedFilesWebpackPlugin({
-              pattern: "src/**/*.*"
-            })
-          ]
-        });
-        compiler.outputFileSystem = new MemoryFS();
-
-        compiler.run((err, stats) => {
-          expect(err).toBeFalsy();
-
-          const { warnings } = stats.compilation;
-          expect(warnings).toHaveLength(1);
-
-          const [unusedFilesError] = warnings;
-          expect(unusedFilesError).toBeInstanceOf(Error);
-
-          const { message } = unusedFilesError;
-          expect(message).toMatchSnapshot();
-
-          done();
-        });
-      },
-      10000
-    );
+    }, 10000);
   });
 
   describe(`options.patterns`, () => {
-    it(
-      `should work as expected for a single pattern`,
-      done => {
-        const compiler = webpack({
-          context: path.resolve(__dirname, `../../`),
-          entry: {
-            UnusedFilesWebpackPlugin: path.resolve(__dirname, `../index.js`)
-          },
-          output: {
-            path: __dirname
-          },
-          plugins: [
-            new UnusedFilesWebpackPlugin({
-              patterns: ["src/**/*.*"]
-            })
-          ]
-        });
-        compiler.outputFileSystem = new MemoryFS();
+    it(`should work as expected for a single pattern`, done => {
+      const compiler = webpack({
+        mode: "none",
+        context: path.resolve(__dirname, `../../`),
+        entry: {
+          UnusedFilesWebpackPlugin: path.resolve(__dirname, `../index.js`)
+        },
+        output: {
+          path: __dirname
+        },
+        plugins: [
+          new UnusedFilesWebpackPlugin({
+            patterns: ["src/**/*.*"]
+          })
+        ]
+      });
+      compiler.outputFileSystem = new MemoryFS();
 
-        compiler.run((err, stats) => {
-          expect(err).toBeFalsy();
+      compiler.run((err, stats) => {
+        expect(err).toBeFalsy();
 
-          const { warnings } = stats.compilation;
-          expect(warnings).toHaveLength(1);
+        const { warnings } = stats.compilation;
+        expect(warnings).toHaveLength(1);
 
-          const [unusedFilesError] = warnings;
-          expect(unusedFilesError).toBeInstanceOf(Error);
+        const [unusedFilesError] = warnings;
+        expect(unusedFilesError).toBeInstanceOf(Error);
 
-          const { message } = unusedFilesError;
-          expect(message).toMatchSnapshot();
+        const { message } = unusedFilesError;
+        expect(message).toMatchSnapshot();
 
-          done();
-        });
-      },
-      10000
-    );
+        done();
+      });
+    }, 10000);
 
-    it(
-      `should work as expected for an array of patterns`,
-      done => {
-        const compiler = webpack({
-          context: path.resolve(__dirname, `../../`),
-          entry: {
-            UnusedFilesWebpackPlugin: path.resolve(__dirname, `../index.js`)
-          },
-          output: {
-            path: __dirname
-          },
-          plugins: [
-            new UnusedFilesWebpackPlugin({
-              patterns: ["src/**/*.*", "!**/__snapshots__/**/*.*"]
-            })
-          ]
-        });
-        compiler.outputFileSystem = new MemoryFS();
+    it(`should work as expected for an array of patterns`, done => {
+      const compiler = webpack({
+        mode: "none",
+        context: path.resolve(__dirname, `../../`),
+        entry: {
+          UnusedFilesWebpackPlugin: path.resolve(__dirname, `../index.js`)
+        },
+        output: {
+          path: __dirname
+        },
+        plugins: [
+          new UnusedFilesWebpackPlugin({
+            patterns: ["src/**/*.*", "!**/__snapshots__/**/*.*"]
+          })
+        ]
+      });
+      compiler.outputFileSystem = new MemoryFS();
 
-        compiler.run((err, stats) => {
-          expect(err).toBeFalsy();
+      compiler.run((err, stats) => {
+        expect(err).toBeFalsy();
 
-          const { warnings } = stats.compilation;
-          expect(warnings).toHaveLength(1);
+        const { warnings } = stats.compilation;
+        expect(warnings).toHaveLength(1);
 
-          const [unusedFilesError] = warnings;
-          expect(unusedFilesError).toBeInstanceOf(Error);
+        const [unusedFilesError] = warnings;
+        expect(unusedFilesError).toBeInstanceOf(Error);
 
-          const { message } = unusedFilesError;
-          expect(message).toMatchSnapshot();
+        const { message } = unusedFilesError;
+        expect(message).toMatchSnapshot();
 
-          done();
-        });
-      },
-      10000
-    );
+        done();
+      });
+    }, 10000);
   });
 });
